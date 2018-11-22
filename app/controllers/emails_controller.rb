b57e03a1480e05f954550e746b753bb52ec93bbf
khalil <>
2018-11-22 20:57:27 +0100

    zeeze

---
+++ app/controllers/emails_controller.rb
@@ -0,0 +1,27 @@
+class EmailsController < ApplicationController
+  def index
+  	@emails = Email.all 
+
+  end
+
+  def show
+    @email = Email.find(params[:id])
+    @email.read = true
+  	  respond_to do |format|
+	    format.html { redirect_to email_path }
+	    format.js
+  	end 
+  end
+
+
+  def destroy
+    @email = Email.find(params[:id])
+    @email.destroy
+    
+    respond_to do |format|
+       format.html { redirect_to email_url }
+       format.json { head :no_content }
+       format.js   { render :layout => false }
+    end
+  end
+end
---
+++ test/controllers/emails_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class EmailsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get emails_index_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get emails_destroy_url
+    assert_response :success
+  end
+
+end
